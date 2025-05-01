a819e08c85ae028d4fd368e3a0700405b61de458
Zachary King <zchrykng@fastmail.com>
2025-05-01 17:30:52 -0400

    Complete edits functions

--- dictionary/edits.go
+++ dictionary/edits.go
@@ -1,44 +1,96 @@
 package dictionary
 
-func (d *Dictionary) edit1(word string) []string {
-	var result []string
+import (
+	mapset "github.com/deckarep/golang-set/v2"
+)
 
-	wordChars := d.WordChars()
-
-	result = append(result, split(word)...)
-
-	return nil
+type pair struct {
+	left  string
+	right string
 }
 
-func split(word string) []string {
-	var results []string
+func edit1(word string, characters string) []string {
+	results := mapset.NewSet[string]()
 
-	for i := 1; i < len(word); i++ {
-		results = append(results, word[:i], word[i:])
+	splits := split(word)
+
+	results.Append(deletes(splits)...)
+	results.Append(transposes(splits)...)
+	results.Append(replacements(splits, characters)...)
+	results.Append(inserts(splits, characters)...)
+
+	return results.ToSlice()
+}
+
+func edit2(word string, characters string) []string {
+	//def edits2(word):
+	//    "All edits that are two edits away from `word`."
+	//    return (e2 for e1 in edits1(word) for e2 in edits1(e1))
+	results := mapset.NewSet[string]()
+
+	for _, edit := range edit1(word, characters) {
+		results.Append(edit1(edit, characters)...)
+	}
+
+	return results.ToSlice()
+}
+
+func split(word string) []pair {
+	var results []pair
+
+	for i := 0; i <= len(word); i++ {
+		results = append(results, pair{word[:i], word[i:]})
 	}
 
 	return results
 }
 
-func deletes(word string) []string {
+func deletes(pairs []pair) []string {
 	var results []string
 
-	for i := 0; i < len(word); i++ {
-		results = append(results, word[:i]+word[i+1:])
+	for _, p := range pairs {
+		if len(p.right) > 0 {
+			results = append(results, p.left+p.right[1:])
+		}
 	}
 
 	return results
 }
 
-//def edits1(word):
-//    "All edits that are one edit away from `word`."
-//    letters    = 'abcdefghijklmnopqrstuvwxyz'
-//    deletes    = [L + R[1:]               for L, R in splits if R]
-//    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
-//    replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
-//    inserts    = [L + c + R               for L, R in splits for c in letters]
-//    return set(deletes + transposes + replaces + inserts)
-//
-//def edits2(word):
-//    "All edits that are two edits away from `word`."
-//    return (e2 for e1 in edits1(word) for e2 in edits1(e1))
+func transposes(pairs []pair) []string {
+	var results []string
+
+	for _, p := range pairs {
+		if len(p.right) > 1 {
+			results = append(results, p.left+string(p.right[1])+string(p.right[0])+p.right[2:])
+		}
+	}
+
+	return results
+}
+
+func replacements(pairs []pair, characters string) []string {
+	var results []string
+
+	for _, p := range pairs {
+		if len(p.right) > 0 {
+			for _, c := range characters {
+				results = append(results, p.left+string(c)+p.right[1:])
+			}
+		}
+	}
+
+	return results
+}
+
+func inserts(pairs []pair, characters string) []string {
+	var results []string
+
+	for _, p := range pairs {
+		for _, c := range characters {
+			results = append(results, p.left+string(c)+p.right)
+		}
+	}
+
+	return results
+}
